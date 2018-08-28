0ac3323d143fa3ef382713eb5d24cc17a851ba77
Julia Balliet <julia.m.balliet@gmail.com>
2018-08-28 14:51:59 -0500

    displayed the info on the DOM

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -4,6 +4,27 @@ class Person extends Component {
 
     constructor() {
         super();
-        this.state = {name: '', role: ''};
+        this.state = { name: '', role: '' };
+    }
+
+    handleNameChange = (event) => {
+        this.setState({ name: event.target.value });
+    }
+
+    handleRoleChange = (event) => {
+        this.setState({ role: event.target.value });
+    }
+
+    render() {
+        return (
+            
+            <div>
+                Name: <input onChange={this.handleNameChange} />
+                Role: <input onChange={this.handleRoleChange} />
+                <br />{this.state.name} is famous for {this.state.role}.
+            </div>
+        );
     }
 }
+
+export default Person;
